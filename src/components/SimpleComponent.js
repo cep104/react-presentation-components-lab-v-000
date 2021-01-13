b7efd3ae596e413568bbe24bd363bf4cf739314a
cep104 <candicelizabethpeters@gmail.com>
2021-01-13 15:29:25 -0600

    commit tree af9acee8700c8c983e1ab79e8e3b45847b02e2ca

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,32 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class ComponentName extends Component {
+  constructor() {
+    super();
+    //does not like putting toggle in state
+    // Initial state here...
+    this.state = {
+      mood: "happy",
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.mood === "happy") {
+      this.setState({
+        mood: "sad",
+      });
+    } else {
+      this.setState({
+        mood: "happy",
+      });
+    }
+    // Probably do some work to update the state
+  };
+
+  render() {
+    //make sur it is on the first return div
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+    // Return JSX that renders into HTML
+  }
+}
