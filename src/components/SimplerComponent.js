f933fc972aeafd30603615a83f66690da97bd0a7
cep104 <candicelizabethpeters@gmail.com>
2021-01-13 15:57:54 -0600

    notes added 2021

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
 // Code SimplerComponent Here
+
+import React from "react";
+//used deconstruction to put in handleClick. props would of worked too
+const SimplerComponent = ({ handleClick }) => {
+  return (
+    //if props used instead it would be this.props.handleClick
+    <div id="words" onClick={handleClick}>
+      I am just happy
+    </div>
+  );
+};
+export default SimplerComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import SimpleComponent from './components/SimpleComponent';
-import SimplerComponent from './components/SimplerComponent';
-
+import SimpleComponent from "./components/SimpleComponent";
+import SimplerComponent from "./components/SimplerComponent";
+//not a class function so JSX rules so need to have let for a function and don't pass in with this.
+let handleClick = () => {
+  let words = document.querySelector("#words");
+  words.style.color = "red";
+};
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={handleClick} />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
